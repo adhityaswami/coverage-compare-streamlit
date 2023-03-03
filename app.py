782a01ad731606b407c0548936cd7d40b6736170
Ubuntu <ubuntu@ip-172-31-24-242.us-east-2.compute.internal>
2023-03-03 11:33:56 +0000

    mutliple chunks

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ import json
 st.title('Coverage Experiments')
 
 data = json.load(open('coverage_experiments.json'))
-exp_names = list(data.keys())
+mapping_dict = {idx: el for idx, el in enumerate(data.keys())}
+indices = list(mapping_dict.keys())
+slider_index = st.slider(label = 'Chunk', min_value = min(indices) + 1, max_value = max(indices) + 1) - 1
+
+chunkData = data[mapping_dict[slider_index]]
+st.text(body = chunkData['text'])
+exp_names = [el for el in list(chunkData.keys()) if 'text' not in el]
 
 tabs = st.tabs(exp_names)
 
 for idx, tab in enumerate(tabs):
     with tab:
-        st.json(sorted(data[exp_names[idx]]))
+        st.json(chunkData[exp_names[idx]])
 
